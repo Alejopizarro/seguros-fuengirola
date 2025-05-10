bf9a1a59f337e352ea48a13cb66fdd212ffd25ff
Alejopizarro <alejomkt10@gmail.com>
2025-05-10 21:15:43 +0200

    fix export data

--- app/(routes)/particulares/seguros-de-salud/page.tsx
+++ app/(routes)/particulares/seguros-de-salud/page.tsx
@@ -4,60 +4,7 @@ import Pricing from "../components/pricing";
 import Carencias from "./components/carencias";
 import Faqs from "../components/faqs";
 import LeadForm from "@/components/lead-form";
-
-export const products = [
-  {
-    title: "DKV Paramedic Plus",
-    description:
-      "El seguro de salud más económico. Hasta 8 miembros de tu familia por el mismo precio.",
-    copago: "Sin copago",
-    price: "Desde 25,50€/mes",
-  },
-  {
-    title: "DKV Profesional",
-    description:
-      "Seguro de salud a medida. Contrata las coberturas que necesites y personaliza tu seguro.",
-    copago: "Disponible con o sin copago",
-    price: "Desde 8€/mes",
-  },
-  {
-    title: "Personal Doctor",
-    description:
-      "Seguro con servicio de médico personal y un asistente de salud para gestionar todos los trámites.",
-    copago: "Disponible con o sin copago",
-    price: "Desde 16€/mes",
-  },
-  {
-    title: "DKV Integral",
-    description:
-      "El seguro de cuadro médico más completo. Asegurate la asistencia sanitaria más cómoda y rápida.",
-    copago: "Disponible con o sin copago",
-    price: "Desde 18€/mes",
-  },
-];
-
-export const faqs = [
-  {
-    question: "¿Qué es un seguro de cuadro médico?",
-    answer:
-      "Cuando hablamos de un seguro de cuadro médico, hacemos referencia al tipo de seguro médico que te permite hacer uso de especialistas y centros médicos concertados. DKV cuenta con un cuadro médico de más de 51.000 profesionales y 1.000 centros concertados en España. El cuadro médico de DKV incluye: Un amplio listado de especialistas médicos: dermatólogos, cardiólogos, cirujanos, ginecólogos, neurólogos, psicólogos, etc. Los mejores hospitales y clínicas del país: DKV cuenta con 8 de los 10 mejores hospitales. Acceso a 23 centros propios de DKV: en nuestros centros médicos propios encontrarás instalaciones amplias, luminosas, accesibles y cómodas.",
-  },
-  {
-    question: "¿Qué es un seguro de salud con reembolso?",
-    answer:
-      "El seguro de salud con reembolso es un tipo de póliza que te permite elegir libremente el médico o el centro médico al que quieres acudir. En este caso, tú decides dónde y cuándo recibir la asistencia médica. Si eliges un médico o un centro no concertado, deberás abonar el importe del servicio y DKV te reembolsará el coste del mismo.",
-  },
-  {
-    question: "¿Qué es un copago?",
-    answer:
-      "El copago es el importe que deberás abonar por cada acto médico realizado, según la modalidad de seguro que tengas contratada. Puedes ver el importe de estos copagos en la «Tabla de Grupos de Actos Médicos y Copagos» de las condiciones particulares y/o especiales de tu póliza.",
-  },
-  {
-    question: "¿Qué es una carencia?",
-    answer:
-      "Es el intervalo de tiempo, contado a partir de la entrada en vigor de la póliza, durante el cual no son efectivas algunas de las coberturas. Puedes consultar las carencias en las condiciones generales y particulares de tu póliza o contactando con la compañía.",
-  },
-];
+import { faqs, products } from "./data";
 
 const Page = () => {
   return (
