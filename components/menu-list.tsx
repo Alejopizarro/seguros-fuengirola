8ac7fe97ae5cd6fb0edece3563a8dbc8347b3958
Alejopizarro <alejomkt10@gmail.com>
2025-05-24 21:05:26 +0200

    title seo in list item menu

--- components/menu-list.tsx
+++ components/menu-list.tsx
@@ -23,7 +23,12 @@ const MenuList = () => {
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               {particulares.map((seguro, index) => (
-                <ListItem key={index} href={seguro.route} title={seguro.title}>
+                <ListItem
+                  key={index}
+                  href={seguro.route}
+                  title={seguro.title}
+                  titleSeo={seguro.titleSeo}
+                >
                   {seguro.description}
                 </ListItem>
               ))}
@@ -55,7 +60,12 @@ const MenuList = () => {
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
               {sobreNosotros.pages.map((page, i) => (
-                <ListItem key={i} href={page.url} title={page.title}>
+                <ListItem
+                  key={i}
+                  href={page.url}
+                  title={page.title}
+                  titleSeo={page.description}
+                >
                   {page.description}
                 </ListItem>
               ))}
@@ -67,30 +77,36 @@ const MenuList = () => {
   );
 };
 
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
+type ListItemProps = React.ComponentPropsWithoutRef<"a"> & {
+  title: string;
+  titleSeo?: string;
+  children?: React.ReactNode;
+};
+
+const ListItem = React.forwardRef<React.ElementRef<"a">, ListItemProps>(
+  ({ className, title, children, titleSeo, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className
+            )}
+            title={titleSeo}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
 ListItem.displayName = "ListItem";
 
 export default MenuList;
